89cdf17e3bc87ea8b673a729b7092584058a3b93
Alex-z01 <alexzchicken@gmail.com>
2022-10-22 15:28:40 -0400

    removed uneeded stuff

--- main.js
+++ main.js
@@ -19,19 +19,6 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 }
 
-ipcMain.handle('dark-mode:toggle', () => {
-  if(nativeTheme.shouldUseDarkColors) {
-    nativeTheme.themeSource = 'light'
-  } else {
-    nativeTheme.themeSource = 'dark'
-  }
-  return nativeTheme.shouldUseDarkColors
-})
-
-ipcMain.handle('dark-mode:system', () => {
-  nativeTheme.themeSource = 'system'
-})
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
