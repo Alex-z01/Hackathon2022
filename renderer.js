19479bd845b57a987b00aa122abbd17ea5d3b816
Alex-z01 <alexzchicken@gmail.com>
2022-10-22 15:10:17 -0400

    Modal

--- renderer.js
+++ renderer.js
@@ -5,12 +5,15 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
- document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
-    const isDarkMode = await window.darkMode.toggle()
-    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
-  })
-  
-  document.getElementById('reset-to-system').addEventListener('click', async () => {
-    await window.darkMode.system()
-    document.getElementById('theme-source').innerHTML = 'System'
-  })
+const modal = document.getElementById('modal_container');
+
+document.getElementById('modal_open').addEventListener('click', async () => {
+  console.log('OPEN');
+  modal.classList.add('show');
+})
+
+document.getElementById('modal_submit').addEventListener('click', async () => {
+  console.log('CLOSE');
+  modal.classList.remove('show');
+})
+
